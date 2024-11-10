5f9eb4ef04c9f4b018aef7de7e7b90a52849395c
Illusion <Algustine@foxmail.com>
2024-11-10 21:03:41 +0800

    Revert "chore: timer only runs under main process"
    This reverts commit 26205648245374c9888dfab967c2750a90354a24.

--- subscription/apps.py
+++ subscription/apps.py
@@ -1,19 +1,17 @@
 from django.apps import AppConfig
 import sys
-import os
 
 class SubscriptionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subscription'
-    initialized = False  # 添加标志变量
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') == 'true':
-            if 'runserver' in sys.argv or 'shell' in sys.argv:
-                from .subscription_checker import SubscriptionChecker
-                from .utils import rate_limiter
-                if not self.initialized:
-                    self.subscription_checker = SubscriptionChecker()
-                    self.rate_limiter = rate_limiter
-                    self.initialized = True
+        if 'runserver' in sys.argv or 'shell' in sys.argv:
+            from .subscription_checker import SubscriptionChecker
+            from .utils import rate_limiter
 
+            # 初始化 SubscriptionChecker
+            self.subscription_checker = SubscriptionChecker()
+            self.rate_limiter = rate_limiter
+
+            print("SubscriptionChecker 已启动。")
